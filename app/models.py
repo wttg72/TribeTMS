2984fddcc3e41a774f156408289b3c4f4093b7d7
wttg72 <wttg72@gmail.com>
2021-07-25 20:40:23 +0800

    migrated model

--- app/models.py
+++ app/models.py
@@ -8,5 +8,27 @@ class Bike(models.Model):
         ('SPO','Sport'),
         ('STR','Street'),
     )
-    number = models.IntegerField()
-    category = models.CharField(max_length=3, choices=BIKE_CATEGORIES)
+
+    BIKE_CLASS=(
+        ('2B','Class 2B'),
+        ('2A','Class 2A'),
+        ('2','Class 2'),
+    )
+
+    BIKE_NAME=(
+        ('Aerox','Yamaha Aerox 155'),
+        ('Sniper','Yamaha Sniper T150'),
+        ('FZ16','Yamaha FZ16'),
+        ('FZ16ST','Yamaha FZ16ST'),
+        ('CB190R','Honda CB190R'),
+        ('CB400 Sp2','Honda CB400 (Super 4) Spec 2'),
+        ('CB400 Sp3','Honda CB400 (Super 4) Spec 3'),
+    )
+
+    plate_no = models.CharField(max_length=8, null=True)
+    category = models.CharField(max_length=3, choices=BIKE_CATEGORIES, null=True)
+    bikeclass = models.CharField(max_length=2, choices=BIKE_CLASS, null=True)
+    bikename = models.CharField(max_length=12, choices=BIKE_NAME, null=True)
+    
+    def __str__(self):
+        return f'{self.number}. {self.bikeclass} {self.category} {self.bikename}'
